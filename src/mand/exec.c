b72db4e3b7aec63689577ecf5c6fb2d02ed66fb7
gonzimaster <oscarpunticoma@gmail.com>
2022-12-16 17:51:25 +0100

    Add envp to execve

--- src/mand/exec.c
+++ src/mand/exec.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 12:24:16 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/12/16 17:16:08 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/12/16 17:51:08 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,49 @@ static char	**get_command_array(t_list *command)
 	return (command_array);
 }
 
+static char	**get_envp(t_list *environ)
+{
+	char	**envp;
+	int		len_list;
+	int		i;
+
+	len_list = ft_lstsize(environ);
+	envp = malloc(sizeof(char *) * (len_list + 1));
+	if (envp == NULL)
+		terminate(ERR_MEM, 1);
+	i = 0;
+	while (environ)
+	{
+		envp[i] = ft_strdup(((t_environ_content *)environ->content)->env_var);
+		if (envp[i] == NULL)
+			terminate(ERR_MEM, 1);
+		i++;
+		environ = environ->next;
+	}
+	envp[i] = NULL;
+	return (envp);
+}
+
+static void do_execve(int *tmp_fd, t_list *command, t_prompt prompt)
+{
+	char	*exec_path;
+	char	**command_array;
+	char	**envp;
+
+	set_child_sigaction();
+	command_array = get_command_array(command);
+	envp = get_envp(prompt.environ);
+	if (get_exec_path(command_array[0], &exec_path, command, &prompt) != 0)
+		exit(((t_cmd_line_content *)command->content)->exit_status);
+	dup2(*tmp_fd, STDIN_FILENO);
+	close(*tmp_fd);
+	execve(exec_path, command_array, 0);
+}
+
 //TODO: Infile and outfile redirections
 static void	do_pipe(int fd[2], int *tmp_fd, t_list *command, t_prompt prompt)
 {
 	pid_t	pid;
-	char	**command_array;
-	char	*exec_path;
 
 	pipe(fd);
 	pid = fork();
@@ -66,22 +103,15 @@ static void	do_pipe(int fd[2], int *tmp_fd, t_list *command, t_prompt prompt)
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
-		set_child_sigaction();
-		command_array = get_command_array(command);
-		if (get_exec_path(command_array[0], &exec_path, command, &prompt) != 0)
-			exit(0);
-		dup2(*tmp_fd, STDIN_FILENO);
-		close(*tmp_fd);
-		execve(exec_path, command_array, 0);
+		do_execve(tmp_fd, command, prompt);
 	}
 }
 
 //TODO: Fix return value of signal sent in pipe that is not last (should be 0)
+//Handle system error exit from child (for example malloc failing in child)
 static int	do_last_command(int *tmp_fd, t_list *command, t_prompt prompt)
 {
 	pid_t	pid;
-	char	*exec_path;
-	char	**command_array;
 	int		exit_status;
 
 	pid = fork();
@@ -109,15 +139,7 @@ static int	do_last_command(int *tmp_fd, t_list *command, t_prompt prompt)
 		}
 	}
 	else if (!pid)
-	{
-		set_child_sigaction();
-		command_array = get_command_array(command);
-		if (get_exec_path(command_array[0], &exec_path, command, &prompt) != 0)
-			exit(((t_cmd_line_content *)command->content)->exit_status);
-		dup2(*tmp_fd, STDIN_FILENO);
-		close(*tmp_fd);
-		execve(exec_path, command_array, 0);
-	}
+		do_execve(tmp_fd, command, prompt);
 	return (0);
 }
 
