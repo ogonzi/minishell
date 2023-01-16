59bdd3ae6d89f3bd84c36d3f5c1ae4ef1614e4d2
ElPatatin <cristobalpes1@gmail.com>
2023-01-16 15:15:36 +0100

    Enviroment finished?

--- mandatory/tools/env.c
+++ mandatory/tools/env.c
@@ -6,7 +6,7 @@
 /*   By: cpeset-c <cpeset-c@student.42barce>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:47:46 by cpeset-c          #+#    #+#             */
-/*   Updated: 2023/01/15 20:52:00 by cpeset-c         ###   ########.fr       */
+/*   Updated: 2023/01/16 14:34:29 by cpeset-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,36 @@
 #define ERRMSG	(char *)"No such file or directory\n"
 #define ENVCMP	(char *)"env"
 
+int	error_env(char *str);
+
 int	main(int ac, char **av, char **ev)
 {
 	ssize_t	idx;
+	t_bool	flag;
+	ssize_t	count;
 
 	idx = -1;
+	flag = TRUE;
+	count = 0;
 	if (ac > 1 && ft_strcmp(ft_strlowcase(av[1]), ENVCMP))
+		return (error_env(av[1]));
+	if (ac > 2)
 	{
-		ft_printf_fd(STDERR_FILENO, "env: %s: %s", av[1], ERRMSG);
-		return (1);
+		while (++count < ac)
+		{
+			if (ft_strcmp(ft_strlowcase(av[count]), ENVCMP))
+				flag = FALSE;
+		}
 	}
-	else if (ac > 1 && !ft_strcmp(ft_strlowcase(av[1]), ENVCMP))
-	{
-		while (ev[++idx])
-			printf("%s\n", ev[idx]);
-		return (0);
-	}
-	else
-	{
-		while (ev[++idx])
-			printf("%s\n", ev[idx]);
-		return (0);
-	}
+	if (flag == FALSE)
+		return (error_env(av[count - 1]));
+	while (ev[++idx])
+		printf("%s\n", ev[idx]);
+	return (0);
+}
+
+int	error_env(char *str)
+{
+	ft_printf_fd(STDERR_FILENO, "env: %s: %s", str, ERRMSG);
 	return (1);
 }
